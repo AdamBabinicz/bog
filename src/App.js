d18e28e34fc0ce06f9d6a03f016a7a26191f14df
babinicz <babinicz@onet.pl>
2022-09-04 19:25:30 +0200

    dodałem cookies

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ import FooterSection from "./components/FooterSection";
 import themes from "./context/theme";
 import Scroll from "./Scroll";
 import ScrollToTop from "./ScrollToTop";
+import CookieConsent from "react-cookie-consent";
 
 function App() {
   const theme = useTheme();
@@ -45,6 +46,33 @@ function App() {
           );
         })}
       </div>
+      <CookieConsent
+        debug={true}
+        location="bottom"
+        style={{
+          background: "#333",
+          textAlign: "left",
+          paddingBottom: "1rem",
+          fontSize: "16px",
+          fontFamily: "Gideon Roman",
+        }}
+        buttonStyle={{
+          color: "#333",
+          background: "#fff",
+          fontSize: "18px",
+          fontFamily: "Gideon Roman",
+          marginRight: "1rem",
+        }}
+        buttonText="OK, rozumiem"
+        expires={365}
+      >
+        "W ramach naszej witryny stosujemy pliki cookies w celu świadczenia
+        Państwu usług na najwyższym poziomie, w tym w sposób dostosowany do
+        indywidualnych potrzeb. Korzystanie z witryny bez zmiany ustawień
+        dotyczących cookies oznacza, że będą one zamieszczane w Państwa
+        urządzeniu końcowym. Możecie Państwo dokonać w każdym czasie zmiany
+        ustawień dotyczących cookies."
+      </CookieConsent>
     </AppStyled>
   );
 }
